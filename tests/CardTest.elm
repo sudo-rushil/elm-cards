ff1d539ef112dc0cc4def09f8e3288e916f87387
sudo-rushil <rushil.mallarapu@gmail.com>
2020-05-13 17:27:35 -0400

    added tests for Cards.elm

--- tests/CardTest.elm
+++ tests/CardTest.elm
@@ -9,10 +9,29 @@ import Test exposing (Test, describe, test)
 -- Test Suite
 
 
-checkSuite : Test
-checkSuite =
-    describe "check project compiles"
-        [ test "check function returns true" <|
+cardSuite : Test
+cardSuite =
+    describe "check we can create cards"
+        [ test "construct ace of spades" <|
             \_ ->
-                Expect.true "hello" <| checkTrue "hello"
+                case new "spades" 1 of
+                    Just card ->
+                        Expect.equal (Card Spades 1) card
+
+                    Nothing ->
+                        Expect.fail "Cards.new failed"
+        , test "invalid card fails" <|
+            \_ ->
+                case new "horses" 1 of
+                    Just card ->
+                        Expect.fail "Invalid card created"
+
+                    Nothing ->
+                        Expect.pass
+        , test "construct ace of spades with default" <|
+            \_ ->
+                Expect.equal (Card Spades 1) <| defaultNew Back "spades" 1
+        , test "invalid card returns default" <|
+            \_ ->
+                Expect.equal Back <| defaultNew Back "horses" 1
         ]
